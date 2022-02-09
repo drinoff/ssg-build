6f02c216e90bbda77a539a5a7324887ec0109afc
Drinoff <drinoff@gmail.com>
2022-02-09 20:44:49 +0100

    logo added styled

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,6 +5,7 @@ import "./Header.css";
 const Header = () => {
     return (
         <header className="header">
+            <h1 className="headerLogo">SSG BUILD</h1>
             <div className="imgWrapper"></div>
             <Nav className="navinHeader" />
             <div className="headerMask"></div>
